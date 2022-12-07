428737deb78889670e8664f6674a630cbde85331
iCell <shawn.lee@binance.com>
2022-12-07 18:06:09 +0900

    Update to swift 5

---
+++ ZoomTransitioning/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  ZoomTransitioning
+//
+//  Created by Shawn on 2022/12/07.
+//  Copyright © 2022 com.shoji. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "ZoomTransitioning",
+    dependencies : []
+)
