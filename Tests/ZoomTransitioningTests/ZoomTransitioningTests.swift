31e8437c09abd30aaa2d460389f3e3d608b7dcc9
iCell <shawn.lee@binance.com>
2022-12-08 10:26:35 +0900

    Support SPM

---
+++ Tests/ZoomTransitioningTests/ZoomTransitioningTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ZoomTransitioning
+
+final class ZoomTransitioningTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ZoomTransitioning().text, "Hello, World!")
+    }
+}
